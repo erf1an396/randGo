72eee30cbefdb7d2eebcae8561ced0a1bd7ed216
erf1an396 <em.karfarma@gmail.com>
2024-02-22 09:14:16 +0330

    function "new" & pointer

--- main.go
+++ main.go
@@ -2,15 +2,32 @@ package main
 
 import "fmt"
 
+type User struct {
+	ID   uint
+	Name string
+}
+
 func main() {
+	i := new(int)
+	fmt.Printf("type of i : %T\n", i)
 
-	i := 0
+	u := new(User)
+	fmt.Printf("type of u : %T\n", u)
 
-start:
-	fmt.Println("i", i)
-	i++
-	if i < 10 {
-		goto start
+	u.ID = 2
+	u.Name = "erfan"
+
+	fmt.Printf("u.id , u.name : %+v, %+v\n", u.ID, u.Name)
+	uu := &User{
+		ID:   1,
+		Name: "erfan",
 	}
-	fmt.Println("Finish")
+	fmt.Printf("type of uu :%T\n", uu)
+
+	var uuu *User
+	fmt.Printf("type of uuu:%T\n", uuu)
+
+	fmt.Println("u.ID", u.ID)
+	fmt.Println("uu.ID", uu.ID)
+	//fmt.Println("uuu.ID", uuu.ID)
 }
